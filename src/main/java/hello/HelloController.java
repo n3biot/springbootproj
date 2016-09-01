964750af7e63d1f94f66e63fba017df02b7776e6
bhannebi <n3biot@gmail.com>
2016-09-01 17:49:31 +0200

    Ajout MetricWriter

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -1,11 +1,23 @@
 package hello;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.actuate.autoconfigure.ExportMetricWriter;
+import org.springframework.boot.actuate.metrics.statsd.StatsdMetricWriter;
+import org.springframework.boot.actuate.metrics.writer.MetricWriter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 public class HelloController {
+    @Value("${spring.application.name:application}.${random.value:0000}")
+    private String prefix = "metrics";
 
+    @Bean
+    @ExportMetricWriter
+    MetricWriter metricWriter() {
+        return new StatsdMetricWriter(prefix, "localhost", 8125);
+    }
     @RequestMapping("/")
     public String index() {
         return "Greetings from Spring Boot!";
