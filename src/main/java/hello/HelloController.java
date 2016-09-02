8a01b1f6b5bbdb221d96763849bfdfd7f5398da8
n3biot <n3biot@gmail.com>
2016-09-02 11:46:41 +0200

    Ajout du GaugeService

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -11,20 +11,26 @@ import org.springframework.boot.actuate.metrics.writer.MetricWriter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.metrics.CounterService;
+import org.springframework.boot.actuate.metrics.GaugeService;
+import org.springframework.stereotype.Service;
 
 @RestController
 public class HelloController {
-    @Value("${spring.application.name:application}.${random.value:0000}")
-    private String prefix = "metrics";
 
+    private double value;
 
-    @Bean
-    @ExportMetricWriter
-    MetricWriter metricWriter() {
-        return new StatsdMetricWriter(prefix, "localhost", 8125);
-    }
+    @Autowired
+    private CounterService counterService;
+    @Autowired
+    private GaugeService gaugeService;
+
     @RequestMapping("/")
     public String index() {
+ 	value=value+1.0;
+        this.counterService.increment("n3biot.demos.reads");
+        this.gaugeService.submit("n3biot.demos.reads",value);
         return "Greetings from Spring Boot!";
     }
 
