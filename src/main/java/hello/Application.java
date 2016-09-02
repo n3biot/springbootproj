8ea8e17e861db185ffb93370096faa93380a071a
n3biot <n3biot@gmail.com>
2016-09-02 11:46:23 +0200

    Simplificaiotn du main

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(Application.class, args);
-
+/*
         System.out.println("Let's inspect the beans provided by Spring Boot:");
 
         String[] beanNames = ctx.getBeanDefinitionNames();
@@ -19,6 +19,7 @@ public class Application {
         for (String beanName : beanNames) {
             System.out.println(beanName);
         }
+*/
     }
 
 }
